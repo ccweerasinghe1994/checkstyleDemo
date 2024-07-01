2da564cd71a09addf082ee6e1a3bb0057883bc4b
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-07-01 08:33:02 +0530

    test

---
+++ src/test/java/com/example/checkstyledemo/CheckstyleDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.checkstyledemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckstyleDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
