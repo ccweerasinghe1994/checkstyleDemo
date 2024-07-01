d6109009ea1e2bddaca30d609df5c5210948f999
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-07-01 08:51:23 +0530

    adding test

--- src/main/java/com/example/checkstyledemo/CheckstyleDemoApplication.java
+++ src/main/java/com/example/checkstyledemo/CheckstyleDemoApplication.java
@@ -2,6 +2,7 @@ package com.example.checkstyledemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.aop.AopAutoConfiguration;
 
 @SpringBootApplication
 public class CheckstyleDemoApplication {
