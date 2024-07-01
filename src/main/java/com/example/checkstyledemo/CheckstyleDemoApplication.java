df74fae4237f3e87176e8c62a65bde0f49d9e071
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-07-01 08:50:13 +0530

    adding test

--- src/main/java/com/example/checkstyledemo/CheckstyleDemoApplication.java
+++ src/main/java/com/example/checkstyledemo/CheckstyleDemoApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CheckstyleDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CheckstyleDemoApplication.class, args);
+		SpringApplication.run(CheckstyleDemoApplication.class,
+				args);
 	}
 
 }
